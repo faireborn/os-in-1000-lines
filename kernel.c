6bc81fe106c345c5a6d15046892cd0b9bbac9780
Ryuto Saito <r.saito0111@gmail.com>
2025-08-12 03:35:09 +0900

    ✨ Malloc

--- kernel.c
+++ kernel.c
@@ -55,7 +55,7 @@ struct trap_frame {
     __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp));                    \
   } while (0)
 
-extern char __bss[], __bss_end[], __stack_top[];
+extern char __bss[], __bss_end[], __stack_top[], __free_ram[], __free_ram_end[];
 
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
                        long arg5, long fid, long eid) {
@@ -79,77 +79,78 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
 void putchar(char ch) { sbi_call(ch, 0, 0, 0, 0, 0, 0, 1); }
 
 __attribute__((naked)) __attribute__((aligned(4))) void kernel_entry(void) {
-  __asm__ __volatile__("csrw sscratch, sp\n"
-                       "addi sp, sp, -4 * 31\n"
-                       "sw ra,  4 * 0(sp)\n"
-                       "sw gp,  4 * 1(sp)\n"
-                       "sw tp,  4 * 2(sp)\n"
-                       "sw t0,  4 * 3(sp)\n"
-                       "sw t1,  4 * 4(sp)\n"
-                       "sw t2,  4 * 5(sp)\n"
-                       "sw t3,  4 * 6(sp)\n"
-                       "sw t4,  4 * 7(sp)\n"
-                       "sw t5,  4 * 8(sp)\n"
-                       "sw t6,  4 * 9(sp)\n"
-                       "sw a0,  4 * 10(sp)\n"
-                       "sw a1,  4 * 11(sp)\n"
-                       "sw a2,  4 * 12(sp)\n"
-                       "sw a3,  4 * 13(sp)\n"
-                       "sw a4,  4 * 14(sp)\n"
-                       "sw a5,  4 * 15(sp)\n"
-                       "sw a6,  4 * 16(sp)\n"
-                       "sw a7,  4 * 17(sp)\n"
-                       "sw s0,  4 * 18(sp)\n"
-                       "sw s1,  4 * 19(sp)\n"
-                       "sw s2,  4 * 20(sp)\n"
-                       "sw s3,  4 * 21(sp)\n"
-                       "sw s4,  4 * 22(sp)\n"
-                       "sw s5,  4 * 23(sp)\n"
-                       "sw s6,  4 * 24(sp)\n"
-                       "sw s7,  4 * 25(sp)\n"
-                       "sw s8,  4 * 26(sp)\n"
-                       "sw s9,  4 * 27(sp)\n"
-                       "sw s10, 4 * 28(sp)\n"
-                       "sw s11, 4 * 29(sp)\n"
+  __asm__ __volatile__(
+      "csrw sscratch, sp\n"
+      "addi sp, sp, -4 * 31\n"
+      "sw ra,  4 * 0(sp)\n"
+      "sw gp,  4 * 1(sp)\n"
+      "sw tp,  4 * 2(sp)\n"
+      "sw t0,  4 * 3(sp)\n"
+      "sw t1,  4 * 4(sp)\n"
+      "sw t2,  4 * 5(sp)\n"
+      "sw t3,  4 * 6(sp)\n"
+      "sw t4,  4 * 7(sp)\n"
+      "sw t5,  4 * 8(sp)\n"
+      "sw t6,  4 * 9(sp)\n"
+      "sw a0,  4 * 10(sp)\n"
+      "sw a1,  4 * 11(sp)\n"
+      "sw a2,  4 * 12(sp)\n"
+      "sw a3,  4 * 13(sp)\n"
+      "sw a4,  4 * 14(sp)\n"
+      "sw a5,  4 * 15(sp)\n"
+      "sw a6,  4 * 16(sp)\n"
+      "sw a7,  4 * 17(sp)\n"
+      "sw s0,  4 * 18(sp)\n"
+      "sw s1,  4 * 19(sp)\n"
+      "sw s2,  4 * 20(sp)\n"
+      "sw s3,  4 * 21(sp)\n"
+      "sw s4,  4 * 22(sp)\n"
+      "sw s5,  4 * 23(sp)\n"
+      "sw s6,  4 * 24(sp)\n"
+      "sw s7,  4 * 25(sp)\n"
+      "sw s8,  4 * 26(sp)\n"
+      "sw s9,  4 * 27(sp)\n"
+      "sw s10, 4 * 28(sp)\n"
+      "sw s11, 4 * 29(sp)\n"
 
-                       "csrr a0, sscratch\n"
-                       "sw a0, 4 * 30(sp)\n"
+      "csrr a0, sscratch\n"
+      "sw a0, 4 * 30(sp)\n"
 
-                       "mv a0, sp\n"
-                       "call handle_trap\n"
+      "mv a0, sp\n" // a0(trap_frame) is the first argument of handle_trap
+      "call handle_trap\n"
 
-                       "lw ra,  4 * 0(sp)\n"
-                       "lw gp,  4 * 1(sp)\n"
-                       "lw tp,  4 * 2(sp)\n"
-                       "lw t0,  4 * 3(sp)\n"
-                       "lw t1,  4 * 4(sp)\n"
-                       "lw t2,  4 * 5(sp)\n"
-                       "lw t3,  4 * 6(sp)\n"
-                       "lw t4,  4 * 7(sp)\n"
-                       "lw t5,  4 * 8(sp)\n"
-                       "lw t6,  4 * 9(sp)\n"
-                       "lw a0,  4 * 10(sp)\n"
-                       "lw a1,  4 * 11(sp)\n"
-                       "lw a2,  4 * 12(sp)\n"
-                       "lw a3,  4 * 13(sp)\n"
-                       "lw a4,  4 * 14(sp)\n"
-                       "lw a5,  4 * 15(sp)\n"
-                       "lw a6,  4 * 16(sp)\n"
-                       "lw a7,  4 * 17(sp)\n"
-                       "lw s0,  4 * 18(sp)\n"
-                       "lw s1,  4 * 19(sp)\n"
-                       "lw s2,  4 * 20(sp)\n"
-                       "lw s3,  4 * 21(sp)\n"
-                       "lw s4,  4 * 22(sp)\n"
-                       "lw s5,  4 * 23(sp)\n"
-                       "lw s6,  4 * 24(sp)\n"
-                       "lw s7,  4 * 25(sp)\n"
-                       "lw s8,  4 * 26(sp)\n"
-                       "lw s9,  4 * 27(sp)\n"
-                       "lw s10, 4 * 28(sp)\n"
-                       "lw s11, 4 * 29(sp)\n"
-                       "lw sp,  4 * 30(sp)\n"
-                       "sret\n");
+      "lw ra,  4 * 0(sp)\n"
+      "lw gp,  4 * 1(sp)\n"
+      "lw tp,  4 * 2(sp)\n"
+      "lw t0,  4 * 3(sp)\n"
+      "lw t1,  4 * 4(sp)\n"
+      "lw t2,  4 * 5(sp)\n"
+      "lw t3,  4 * 6(sp)\n"
+      "lw t4,  4 * 7(sp)\n"
+      "lw t5,  4 * 8(sp)\n"
+      "lw t6,  4 * 9(sp)\n"
+      "lw a0,  4 * 10(sp)\n"
+      "lw a1,  4 * 11(sp)\n"
+      "lw a2,  4 * 12(sp)\n"
+      "lw a3,  4 * 13(sp)\n"
+      "lw a4,  4 * 14(sp)\n"
+      "lw a5,  4 * 15(sp)\n"
+      "lw a6,  4 * 16(sp)\n"
+      "lw a7,  4 * 17(sp)\n"
+      "lw s0,  4 * 18(sp)\n"
+      "lw s1,  4 * 19(sp)\n"
+      "lw s2,  4 * 20(sp)\n"
+      "lw s3,  4 * 21(sp)\n"
+      "lw s4,  4 * 22(sp)\n"
+      "lw s5,  4 * 23(sp)\n"
+      "lw s6,  4 * 24(sp)\n"
+      "lw s7,  4 * 25(sp)\n"
+      "lw s8,  4 * 26(sp)\n"
+      "lw s9,  4 * 27(sp)\n"
+      "lw s10, 4 * 28(sp)\n"
+      "lw s11, 4 * 29(sp)\n"
+      "lw sp,  4 * 30(sp)\n"
+      "sret\n");
 }
 
 void handle_trap(struct trap_frame *f) {
@@ -161,11 +162,28 @@ void handle_trap(struct trap_frame *f) {
         user_pc);
 }
 
+paddr_t alloc_pages(uint32_t n) {
+  static paddr_t next_paddr =
+      (paddr_t)__free_ram; // This line is executed only first call
+  paddr_t paddr = next_paddr;
+  next_paddr += n * PAGE_SIZE;
+
+  if (next_paddr > (paddr_t)__free_ram_end)
+    PANIC("out of memory");
+
+  memset((void *)paddr, 0, n * PAGE_SIZE); // Memory Initialization
+  return paddr;
+}
+
 void kernel_main(void) {
   memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
 
-  WRITE_CSR(stvec, (uint32_t)kernel_entry);
-  __asm__ __volatile__("unimp");
+  paddr_t paddr0 = alloc_pages(2);
+  paddr_t paddr1 = alloc_pages(1);
+  printf("alloc_pages test: paddr0=%x\n", paddr0);
+  printf("alloc_pages test: paddr1=%x\n", paddr1);
+
+  PANIC("booted!");
 }
 
 __attribute__((section(".text.boot"))) __attribute__((naked)) void boot(void) {
