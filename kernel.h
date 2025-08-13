f8e9ba95875c1ba0349eccb43010d8c2bea3fb4b
Ryuto Saito <r.saito0111@gmail.com>
2025-08-13 21:58:53 +0900

    ✨ Process

--- kernel.h
+++ kernel.h
@@ -1,6 +1,20 @@
-#pragma once
+#define PROCS_MAX 8
 
-struct sbiret {
-  long error;
-  long value;
+#define PROC_UNUSED 0
+#define PROC_RUNNABLE 1
+
+#include "common.h"
+
+struct sbiret
+{
+        long error;
+        long value;
+};
+
+struct process
+{
+        int pid;
+        int state;
+        vaddr_t sp;
+        uint8_t stack[8192];
 };
